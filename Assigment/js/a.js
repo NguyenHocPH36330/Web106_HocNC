4272585c3382e64afa56eb8cc2521b48139f5409
unknown <hocncph36330@fpt.edu.vn>
2023-03-28 23:09:50 +0700

    ok

---
+++ Assigment/js/a.js
@@ -0,0 +1,9 @@
+function opennn() {
+    var x = document.getElementById("menu2");
+
+    if (x.className === "none") {
+        x.className += " show";
+    } else {
+        x.className = "none";
+    }
+}
